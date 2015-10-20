19090918ac2e1411b43c069eff591bd837a36da5
michallaskowski <michal.laskowski.dev@gmail.com>
2015-10-20 17:57:41 +0200

    Add back empty UnitTests
    Trying to make Travis happy

---
+++ Example/FeetInchDelegate_Tests/FeetInchDelegate_Tests.swift
@@ -0,0 +1,28 @@
+//
+//  FeetInchDelegate_Tests.swift
+//  FeetInchDelegate_Tests
+//
+//  Created by mlaskowski on 20/10/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class FeetInchDelegate_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
